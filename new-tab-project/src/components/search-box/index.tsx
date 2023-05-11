7f5c44f27cfc785c4ca650b9395a950c90aa01a9
cat-sky <crazy.husky.c@outlook.com>
2023-05-11 17:11:37 +0800

    搜索引擎基本行为及结构完成，还缺少样式 	new file:   new-tab-project/src/assets/error.svg 	new file:   new-tab-project/src/components/search-box/engine-switch-item.tsx 	modified:   new-tab-project/src/components/search-box/index.tsx 	modified:   new-tab-project/src/components/search-box/search-button.tsx 	modified:   new-tab-project/src/components/search-box/search-engine-switch.tsx 	new file:   new-tab-project/src/read.md 	new file:   new-tab-project/src/store/search-state.ts 	new file:   "new-tab-project/src/store/\345\212\237\350\203\275\344\273\213\347\273\215.md" 	new file:   new-tab-project/src/types/propSearch.ts 	new file:   new-tab-project/src/utils/keyDownHandle.ts 	modified:   new-tab-project/src/utils/search.ts 	new file:   prove.ts

--- new-tab-project/src/components/search-box/index.tsx
+++ new-tab-project/src/components/search-box/index.tsx
@@ -1,13 +1,55 @@
+import { useReducer, useState } from "react";
+
 import SearchButton from "./search-button";
 import SearchEngineSwitch from "./search-engine-switch";
-
+import { searchReducer,getInitsearchEngines } from '@/store/search-state'
+import { getFormatSearch } from "@/utils/search";
 export default function SearchBox() {
-
+    const [searchEngines, dispatch] = useReducer(searchReducer, getInitsearchEngines());
+    //搜索引擎列表的reducer
+    const [searchWord,setSearchWord]=useState('');
+    //搜索关键词
+    const [count, setCount] = useState(0);
+    //当前选择哪个搜索引擎？
+    function onAccountChange(e:any){
+        //同步输入词
+        const value = e.target.value;
+        setSearchWord(value);
+    }
+    const SearchKeyHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
+        //监听键盘事件
+        switch (event.code) {
+            case "Enter":
+                window.location.replace(
+                    getFormatSearch(searchWord,searchEngines[count].url));
+                break;
+        
+            default:
+                break;
+        }
+    };
     return (
-        <div className="search-box">
-            <SearchEngineSwitch/>
-            <input type="text" />
-            <SearchButton/>
+        <div className="search-box" 
+        
+        >
+            {count}
+            <SearchEngineSwitch 
+            searchEngines={searchEngines}
+            optEngines={dispatch}
+            setEngine={[count,setCount]}
+            />
+            <input 
+            type="text" 
+            value={searchWord} 
+            onChange={onAccountChange}
+            onKeyDown={SearchKeyHandler}
+            />
+            <SearchButton clickfn={()=>{
+                window.location.
+                replace(getFormatSearch(searchWord,
+                searchEngines[count].url))
+            }}/>
         </div>
     )
 }
+
--- new-tab-project/src/utils/search.ts
+++ new-tab-project/src/utils/search.ts
@@ -1,3 +1,10 @@
 export function getSiteIco(params:string) {
-    return `${params}/favicon.ico`
+    const url=params.slice(0,params.indexOf('com')+3);
+    return `${url}/favicon.ico`
+}
+ 
+export function getFormatSearch(word:string,url:string){
+    const start=url.slice(0,url.indexOf('%s'));
+    const end=url.slice(url.indexOf('%s')+2,url.length);
+    return `${start+word+end}`;
 }
